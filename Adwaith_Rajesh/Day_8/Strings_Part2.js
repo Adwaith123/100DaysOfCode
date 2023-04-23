d8e0f4f973221bbd2329cab2c0f5c3cda25c17f9
Adwaith123 <zerotomastery123@gmail.com>
2023-04-23 14:00:06 +0530

    updated strigs2

--- Adwaith_Rajesh/Day_8/Strings_Part2.js
+++ Adwaith_Rajesh/Day_8/Strings_Part2.js
@@ -27,3 +27,49 @@ const normalizedEmail = loginEmail.toLowerCase().trim();
 // How the above line work   [loginEmail.toLowerCase()]this will return a string and then trimmed method is used on that string.trim();
 
 console.log(normalizedEmail); //hello@jonas.io
+
+console.log(email === normalizedEmail);//true
+
+//Replacing
+
+const priceGB = '288,97€';
+const priceUS = priceGB.replace('€', '$').replace(',', '.');
+console.log(priceUS);
+
+const announcement =
+  'All passengers come to boarding door 23.Boarding door 23!';
+
+console.log(announcement.replace('door', 'gate'));
+//All passengers come to boarding gate 23.Boarding door 23!
+console.log(announcement.replaceAll('door', 'gate'));
+//All passengers come to boarding gate 23.Boarding gate 23!
+
+// -----Using regular expression
+console.log(announcement.replace(/door/g, 'gate'));
+//All passengers come to boarding gate 23.Boarding gate 23!
+
+//Booleans
+
+const planes = 'Airbus A320neo';
+console.log(planes.includes('A320')); //true
+console.log(planes.includes('Boeing')); //false
+console.log(planes.startsWith('Air')); //true
+
+if (planes.startsWith('Airbus') && planes.endsWith('neo')) {
+  console.log('Part of the new Airbus');
+}
+
+//Practice exercise
+
+const checkBaggage = function (items) {
+  const baggage = items.toLowerCase();
+  if (baggage.includes('knife') || baggage.includes('gun')) {
+    console.log('You are not allowed to get on board');
+  } else {
+    console.log('Welcome Abroad');
+  }
+};
+
+checkBaggage('I have a laptop , some food and a pocket Knife');
+checkBaggage('Socks and camera');
+checkBaggage('Got some snacks and a gun for protection');
