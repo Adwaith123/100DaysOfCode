bdf3564ba567ed92a2385ead6bbfdd01cd8444a7
Adwaith123 <zerotomastery123@gmail.com>
2023-04-27 19:34:17 +0530

    Added IIFE

---
+++ Adwaith_Rajesh/Day__11/Immediately_Invoked_Function_expression.js
@@ -0,0 +1,25 @@
+//-----------------Immediately invoked function expression
+
+const runOnce = function () {
+  console.log('This will never run again');
+};
+
+runOnce();
+
+(function () {
+  console.log('This will never run again'); //This body is called Immediately invoked function expression
+  const isPrivate = 23;
+})();
+
+(() => console.log('This will also never run again'))();
+
+{
+  const isPrivate = 23;
+  var notPrivate = 46;
+}
+console.log(isPrivate); //Will create block scope
+console.log(notPrivate); // Does not create a block scope
+
+//---Note : Here the Immediately invoked funcctio is not still used anyway beacuse a block of statemnet inside "{ }" can create a scope for private variables(privacy)..So inorder to create privately accessable varibles we nowadays do not relay on IIMe.
+
+// It is still used now a days for executing this function only once ..And its most use case can be see in async javascript
